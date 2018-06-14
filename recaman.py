89a8830971ea0d38e654a45d4c664d6932bb8a8d
whb <whbdupree@gmail.com>
2018-06-14 17:46:26 -0400

    lets do a concurrency

--- recaman.py
+++ recaman.py
@@ -1,17 +1,33 @@
-s = 0
-x = [1+s]
-def recaman(a,n):
-    b = a - (n+s)
-    if b > 0:
-        try:
-            x.index(b)
-            b = a + (n+s)
-        except ValueError:
-            pass
-    else:
-        b = a + (n+s)
-    x.append(b)
+from threading import Thread
+import numpy as np
 
-for i in range(20):
-    recaman(x[-1],i+2)
-print(x)
+class recaman(Thread): 
+    def __init__(self,s,q):
+        self.s = s # offset
+        self.x = [1+s] # list holds sequence starting at 1+s
+        self.q = q # how may steps
+        super(recaman, self).__init__()
+    def eval(self,a,n):
+        ns = n+self.s # might use it twice
+        b = a - ns
+        if b > 0:
+            try:
+                self.x.index(b)
+                b = a + ns
+            except ValueError:
+                pass
+        else:
+            b = a + ns
+        self.x.append(b)
+    def run(self):
+        for i in range(self.q):
+            self.eval(self.x[-1],i+2)
+
+
+
+rlist = [ recaman(n,20) for n in range(5) ]
+for r in rlist:
+    r.start()
+for r in rlist:
+    r.join()
+    print(r.x)
