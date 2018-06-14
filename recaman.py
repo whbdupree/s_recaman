0d42fd850b2c301f2dd194362130bb090fc048d0
whb <whbdupree@gmail.com>
2018-06-14 16:40:19 -0400

    a_1 is offset, too

--- recaman.py
+++ recaman.py
@@ -1,5 +1,5 @@
-s = 5
-x = [1]
+s = 0
+x = [1+s]
 def recaman(a,n):
     b = a - (n+s)
     if b > 0:
@@ -12,6 +12,6 @@ def recaman(a,n):
         b = a + (n+s)
     x.append(b)
 
-for i in range(10):
+for i in range(20):
     recaman(x[-1],i+2)
 print(x)
